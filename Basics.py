479c87ea6c35e1a7952bbdfb0419c450ceb040bb
Achref ZNAIDIA <achref.znaidia@gmail.com>
2022-03-27 16:48:03 +0200

    start uml

--- Basics.py
+++ Basics.py
@@ -1,1 +1,16 @@
+import keyword
+import math
 
+
+class Basics(object):
+    def __init__(self, x):
+        self.x
+        
+    def printFunc(x):
+        print(keyword.kwlist)
+        print(dir(math))
+        print(x)
+        print('pythoneer')
+
+
+Basics.printFunc(5)
