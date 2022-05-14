47a1e8a23360b68af20f38caa92baa88b194ce54
Achref ZNAIDIA <achref.znaidia@gmail.com>
2022-05-14 18:24:22 +0200

    readme file

--- Basics.py
+++ Basics.py
@@ -1,16 +1,40 @@
-import keyword
-import math
-
+import json
 
 class Basics(object):
-    def __init__(self, x):
-        self.x
+    def __init__(self):
+        pass
         
-    def printFunc(x):
-        print(keyword.kwlist)
-        print(dir(math))
-        print(x)
-        print('pythoneer')
+    def printjsonnodes():
+        with open("config.json","r") as file:
+            data = json.load(file)
+            iterations = len(data.get("event"))
+            print(iterations)
+            for node in data.get("event"):
+                print(node)
+        file.close()
 
 
-Basics.printFunc(5)
+    def removejsonnodes():
+        with open("config.json", "r+") as file:
+            data = json.load(file)
+            data.get("event").clear()
+            file.seek(0)
+            file.truncate()
+            json.dump(data,file)
+        file.close()
+
+    def revertjsonnodes():
+        with open("config.json", "r+") as file:
+            data = {"event": [{"message": "kernel","status": "yes"},{"message": "audit","status": "yes"},{"message": "sysl","status": "yes"},{"message": "journal","status": "yes"}]}
+            file.seek(0)
+            file.truncate()
+            json.dump(data,file)
+        file.close()
+
+
+
+Basics.printjsonnodes()
+Basics.removejsonnodes()
+Basics.printjsonnodes()
+Basics.revertjsonnodes()
+Basics.printjsonnodes()
