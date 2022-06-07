a55ca2384b23ce56317f7ddf2d76b89179950284
Achref ZNAIDIA <achref.znaidia@gmail.com>
2022-06-07 06:22:39 +0200

    basics

---
+++ Lists_manager.py
@@ -0,0 +1,41 @@
+
+
+class Lists_manager(object):
+    def __init__(self):
+        pass
+
+    def isEmpty(self):
+        pass
+
+    def isMixed(self):
+        pass
+
+    def isNested(self):
+        pass
+
+    def printListe(self):
+        pass
+
+    def appendList(self):
+        pass
+
+    def insertInList(self):
+        pass
+
+    def removeFromList(self):
+        pass
+
+    def indexOfItem(self):
+        pass
+
+    def lengthOfList(self):
+        pass
+
+    def countItemInList(self):
+        pass
+
+    def reverseList(self):
+        pass
+
+    def popList(self):
+        pass
