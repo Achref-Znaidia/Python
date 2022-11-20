c431baa19380dbab010337cbd1fea872495609e5
Achref ZNAIDIA <achref.znaidia@gmail.com>
2022-11-20 10:26:22 +0100

    test_framework json_manager

--- Development/Test_Framework/Libraries/Json_manager/json_manager.py
+++ Development/Test_Framework/Libraries/Json_manager/json_manager.py
@@ -6,17 +6,44 @@ class json_manager():
     def __init__(self):
         pass
 
-    def read_json_file(self, json_file_content):
+    def read_json_file(self, json_file_path):
+        # open json file
+        json_file = open(json_file_path, "r")
+
+        # read json file content
+        json_file_content = json_file.read()
+
+        # close json file
+        json_file.close()
+
         # parse json file into json object:
         return json.loads(json_file_content)
 
-    def write_into_json_file(self, json_content):
-        # convert json object into json file:
-        return json.dumps(json_content, indent=4, separators=(", ", " = "))
+    def write_into_json_file(self, json_file_path, json_content):
+        # open json file
+        json_file = open(json_file_path, "w")
 
-    def print_json_content(self, printed_value):
+        # convert json object into json file:
+        json_file_content = json.dumps(json_content, indent=4, separators=(", ", " = "))
+
+        # write json content into the file
+        json_file.write(json_file_content)
+
+        # close json file
+        json_file.close()
+
+    def print_json_file_content(self, json_file_path):
+        # open json file
+        json_file = open(json_file_path, "r")
+
+        # read json file content
+        json_file_content = json_file.read()
+
+        # close json file
+        json_file.close()
+
         # print content
-        print("the content of json file :\n", printed_value)
+        print("the content of json file :\n", json_file_content)
 
     def modify_node_json_file(self):
         pass
@@ -25,6 +52,12 @@ class json_manager():
         pass
 
     def remove_node_json_file(self):
+        pass
+
+    def create_json_file(self):
+        pass
+
+    def delete_json_file(self):
         pass
 
 
@@ -38,8 +71,10 @@ json_content = {
   "age": 40,
   "city": "Las Vegas"
 }
+File_path = "C:/Python repositry/Python/config.json"
+File_path1 = "C:/Python repositry/Python/config1.json"
 json_manager = json_manager()
-x = json_manager.read_json_file(json_file_content)
-json_manager.print_json_content(x)
-y = json_manager.write_into_json_file(json_content)
-json_manager.print_json_content(y)
+x = json_manager.read_json_file(json_file_path=File_path)
+print(x)
+json_manager.write_into_json_file(json_file_path=File_path1, json_content=x)
+json_manager.print_json_file_content(json_file_path=File_path1)
