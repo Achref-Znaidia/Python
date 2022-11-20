e8a7860efcf7f63a9f397b55097e23c683e1e3fd
Achref ZNAIDIA <achref.znaidia@gmail.com>
2022-11-20 10:40:15 +0100

    test_framework json_manager

--- Development/Test_Framework/Libraries/Json_manager/json_manager.py
+++ Development/Test_Framework/Libraries/Json_manager/json_manager.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 
 class json_manager():
@@ -54,11 +55,34 @@ class json_manager():
     def remove_node_json_file(self):
         pass
 
-    def create_json_file(self):
-        pass
+    def create_json_file(self, json_file_path):
 
-    def delete_json_file(self):
-        pass
+        json_content = {}
+
+        if not os.path.exists(json_file_path):
+            # open json file
+            json_file = open(json_file_path, "w")
+
+            # convert json object into json file:
+            json_file_content = json.dumps(json_content, indent=4, separators=(", ", " = "))
+
+            # write json content into the file
+            json_file.write(json_file_content)
+
+            # close json file
+            json_file.close()
+            print(f"The file {json_file_path} is created")
+        else:
+            print(f"The file {json_file_path} exist before")
+
+
+
+    def delete_json_file(self, json_file_path):
+        if os.path.exists(json_file_path):
+            os.remove(json_file_path)
+            print(f"The file {json_file_path} has been deleted")
+        else:
+            print(f"The file {json_file_path} does not exist")
 
 
 json_file_content = """{
@@ -73,8 +97,12 @@ json_content = {
 }
 File_path = "C:/Python repositry/Python/config.json"
 File_path1 = "C:/Python repositry/Python/config1.json"
+File_path2 = "C:/Python repositry/Python/config2.json"
 json_manager = json_manager()
 x = json_manager.read_json_file(json_file_path=File_path)
 print(x)
+json_manager.create_json_file(File_path2)
 json_manager.write_into_json_file(json_file_path=File_path1, json_content=x)
 json_manager.print_json_file_content(json_file_path=File_path1)
+json_manager.delete_json_file(json_file_path=File_path1)
+json_manager.delete_json_file(json_file_path=File_path2)
