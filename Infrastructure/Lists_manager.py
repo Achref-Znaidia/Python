ef8b10c7b93972196c24c3420665cd104c5e4fc7
Achref-Znaidia <achref.znaidia@gmail.com>
2023-12-02 17:56:29 +0100

    Infrastructure
    Lists_manager

--- Infrastructure/Lists_manager.py
+++ Infrastructure/Lists_manager.py
@@ -4,8 +4,14 @@ class Lists_manager(object):
     def __init__(self):
         pass
 
-    def isEmpty(self):
-        pass
+    def isEmpty(self, _list):
+        print("Checking if the list is empty\n")
+        if len(_list) == 0:
+            print("The list is empty\n")
+            return True
+        else:
+            print("The list is not empty\n")
+            return False
 
     def isMixed(self):
         pass
@@ -13,13 +19,14 @@ class Lists_manager(object):
     def isNested(self):
         pass
 
-    def printListe(self):
-        pass
+    def printList(self, _list):
+        print(f"The list content: {_list}")
 
-    def appendList(self):
-        pass
+    def appendList(self, _list1, _list2):
+        print("Appending a list to another")
+        _list1.append(_list2)
 
-    def insertInList(self):
+    def insertInList(self, _list, _index):
         pass
 
     def removeFromList(self):
